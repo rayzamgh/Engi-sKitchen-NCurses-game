a48733355fc511ea4a75acffc52e8721934ebfee
rayzamgh <rayzaganteng@gmail.com>
2018-11-04 17:09:31 +0700

    Update stage.c

---
+++ stage.c
@@ -0,0 +1,22 @@
+//ADT STAGE GAME
+#include "matriks.h"
+#include "boolean.h"
+#include <stdio.h>
+
+#define BLANK ' '
+
+typedef struct {
+	MATRIKS Kitchen;
+	MATRIKS Area1;
+	MATRIKS Area2;
+	MATRIKS Area3;
+}STAGE;
+
+CreateEmpty(*St STAGE)
+//inisiasi stage menjadi matriks char BLANK 8x8 di semua area 
+{
+	MakeMATRIKS(int NB, int NK, MATRIKS * M);
+	MakeMATRIKS(int NB, int NK, MATRIKS * M);
+	MakeMATRIKS(int NB, int NK, MATRIKS * M);
+	MakeMATRIKS(int NB, int NK, MATRIKS * M);
+}
